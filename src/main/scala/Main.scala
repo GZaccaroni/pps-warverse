9a406cd559bfd67953c60a4c0d24c06a0614517d
GZaccaroni <GZaccaroni@users.noreply.github.com>
2022-10-22 10:06:29 +0000

    Apply automatic changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,10 @@
-@main def hello(): Unit = {
+@main def hello(): Unit =
 
-
-  
   println("Hello world!")
-
 
   println(msg);
   println("Test p4 4");
 
-
-
   println("Test 8");
-
-
-  
-}
-
-
-
-
-
-
-
-
-
-
-
 
 def msg = "I was compiled by Scala 3. :)"
