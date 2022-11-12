d6f1587de052712403b71138d10215b2366822f7
Dallas <71103219+LeleDallas@users.noreply.github.com>
2022-11-12 12:20:16 +0100

    test: Added missing Launcher Test

---
+++ app/src/test/scala/it/unibo/warverse/LauncherTest.scala
@@ -0,0 +1,14 @@
+package it.unibo.warverse
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+import javax.swing.JPanel
+import javax.swing.JFrame
+
+class LauncherTest extends AnyFunSuite with Matchers:
+  test("A MainFrame is an instance of JFrame and is not null") {
+    val mainFrame = new MainFrame()
+    assert(mainFrame.isInstanceOf[JFrame])
+    assert(mainFrame != null)
+  }
