835315762a2a9f5e3330f648c1e25b41f24f6cf0
Dallas <71103219+LeleDallas@users.noreply.github.com>
2022-11-14 12:47:11 +0100

    refactor: Create Map States with World Object

--- app/src/main/scala/it/unibo/warverse/ui/view/GameMap.scala
+++ app/src/main/scala/it/unibo/warverse/ui/view/GameMap.scala
@@ -14,26 +14,46 @@ import java.awt.geom.Ellipse2D
 import javax.swing.JPopupMenu
 import javax.swing.JTextField
 import java.awt.Toolkit
-import java.awt.geom.Point2D
+import it.unibo.warverse.model.common.Geometry.Point2D
+import it.unibo.warverse.model.common.Geometry.Polygon2D
+import it.unibo.warverse.model.world.World.Country
+import it.unibo.warverse.model.world.World.Citizen
+import it.unibo.warverse.model.common.Geometry
+import java.awt.Polygon
 
 class GameMap extends JPanel with MouseMotionListener:
   // 1400 762
-  val stateMap: Array[Array[Point2D.Double]] = Array(
+  val countries: Array[Country] = Array(
     // STATE 1
-    Array(
-      new Point2D.Double(150, 150),
-      new Point2D.Double(250, 100),
-      new Point2D.Double(325, 125),
-      new Point2D.Double(375, 225),
-      new Point2D.Double(450, 250),
-      new Point2D.Double(275, 375),
-      new Point2D.Double(100, 300)
+    new Country(
+      "Test1",
+      List(new Citizen(new Geometry.Point2D(0, 0))),
+      null,
+      0.0,
+      Polygon2D(
+        List(
+          Point2D(150, 150),
+          Point2D(250, 100),
+          Point2D(325, 125),
+          Point2D(375, 225),
+          Point2D(450, 250),
+          Point2D(275, 375),
+          Point2D(100, 300)
+        )
+      )
     ),
-    // STATE 2
-    Array(
-      new Point2D.Double(550, 550),
-      new Point2D.Double(650, 500),
-      new Point2D.Double(925, 600)
+    new Country(
+      "Test2",
+      List(new Citizen(new Geometry.Point2D(0, 0))),
+      null,
+      0.0,
+      Polygon2D(
+        List(
+          Point2D(550, 550),
+          Point2D(650, 500),
+          Point2D(925, 600)
+        )
+      )
     )
   )
   val start = new Rectangle(0, 0, 200, 200)
@@ -42,13 +62,11 @@ class GameMap extends JPanel with MouseMotionListener:
   val NUM_COLS = 120
   val NUM_ROWS = 240
   var TERRAIN = Array(
-    new Color(255,255,0),
-    
+    new Color(255, 255, 0)
   )
   val terrainGrid: Array[Array[Color]] =
     Array.ofDim[Color](NUM_ROWS + 1, NUM_COLS + 1)
 
-  
   override def getPreferredSize(): Dimension =
     return Toolkit.getDefaultToolkit().getScreenSize()
   override def mouseDragged(e: MouseEvent): Unit = mouseMoved(e)
@@ -62,18 +80,15 @@ class GameMap extends JPanel with MouseMotionListener:
     g.clearRect(0, 0, getWidth(), getHeight())
     val rectWidth: Int = 5
     val rectHeight: Int = 5
-
     var g2d: Graphics2D = g.asInstanceOf[Graphics2D]
-    stateMap.foreach(singleState =>
-      var xPoly: Array[Int] = Array()
-      var yPoly: Array[Int] = Array()
-      singleState.foreach((coordinates: Point2D.Double) =>
-        xPoly :+= coordinates.x.toInt
-        yPoly :+= coordinates.y.toInt
-       
-      )
-      g2d.setColor(new Color(0,0,255))
-      g2d.fillPolygon(xPoly, yPoly, xPoly.length)
+    var xPoly: Array[Int] = Array()
+    var yPoly: Array[Int] = Array()
+    countries.foreach(country =>
+      val polygon = new Polygon
+      val pointList = country.boundaries.vertexes
+      pointList.foreach(point => polygon.addPoint(point.x.toInt, point.y.toInt))
+      g2d.fillPolygon(polygon)
     )
+
   this.setPreferredSize(getPreferredSize())
   this.requestFocus()
