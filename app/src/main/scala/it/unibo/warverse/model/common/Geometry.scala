8ea7b7d19d146ddba1f3697924ef0bc9ade3cea0
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-17 18:11:49 +0100

    feat: Added Geometry class with movement implementation

--- app/src/main/scala/it/unibo/warverse/model/common/Geometry.scala
+++ app/src/main/scala/it/unibo/warverse/model/common/Geometry.scala
@@ -1,9 +1,58 @@
 package it.unibo.warverse.model.common
 
+import it.unibo.warverse.model.common.Math.DoubleWithAlmostEquals
+import java.awt.geom as AwtGeom
+
 object Geometry:
+
+  trait Point[PointType <: Point[PointType]]:
+    def distanceFrom(point: PointType): Double
+
+  trait MovablePoint[PointType <: Point[PointType]]:
+    def moved(toward: PointType, of: Double): PointType
+
   case class Point2D(x: Double, y: Double)
+      extends Point[Point2D],
+        MovablePoint[Point2D]:
+    override def distanceFrom(point: Point2D): Double =
+      math.sqrt(math.pow(point.x - x, 2) + math.pow(point.y - y, 2))
+    override def equals(point: Any): Boolean =
+      given Math.Precision(0.1)
+      point match
+        case point: Point2D => (point.x ~= x) && (point.y ~= y)
 
-  trait Polygon[Vertex](vertexes: List[Vertex])
+    override def moved(toward: Point2D, of: Double): Point2D =
+      (toward.x - x, toward.y - y) match
+        case (0, 0)                  => Point2D(x, y)
+        case (0, yDiff) if yDiff > 0 => Point2D(x, y + of.min(yDiff))
+        case (0, yDiff) if yDiff < 0 => Point2D(x, y + (-of).max(yDiff))
+        case (xDiff, 0) if xDiff > 0 => Point2D(x + of.min(xDiff), y)
+        case (xDiff, 0) if xDiff < 0 => Point2D(x + (-of).max(xDiff), y)
+        case (xDiff, yDiff) =>
+          val alpha = math.atan2(yDiff, xDiff)
+          val distance = distanceFrom(toward)
+          val movement = math.min(distance, of)
+          val yMovement = movement * math.sin(alpha)
+          val xMovement = yMovement / math.tan(alpha)
+          Point2D(x + xMovement, y + yMovement)
+  trait Polygon[Point]:
+    def vertexes: List[Point]
+    def contains(point: Point): Boolean
 
-  case class Polygon2D(vertexes: List[Point2D])
-      extends Polygon[Point2D](vertexes)
+  type Polygon2D = Polygon[Point2D]
+  object Polygon2D:
+    def apply(vertexes: List[Point2D]): Polygon2D = JavaAwtPolygon2D(vertexes)
+
+  case class JavaAwtPolygon2D(vertexes: List[Point2D]) extends Polygon2D:
+    override def contains(point: Point2D): Boolean =
+      awtPath2D.contains(point.x, point.y) || vertexes.contains(point);
+
+    private def awtPath2D: AwtGeom.Path2D =
+      val path2D = AwtGeom.Path2D.Double()
+      vertexes.headOption match
+        case Some(firstVertex) =>
+          path2D.moveTo(firstVertex.x, firstVertex.y)
+          vertexes.tail
+            .foreach(vertex => path2D.lineTo(vertex.x, vertex.y))
+          path2D
+        case None => path2D
---
+++ app/src/test/scala/it/unibo/warverse/model/common/GeometryTest.scala
@@ -0,0 +1,98 @@
+package it.unibo.warverse.model.common
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+import it.unibo.warverse.model.common.Geometry
+import org.scalatest.Suites;
+
+object GeometryTest:
+  class Point2DTest() extends AnyFunSuite with Matchers:
+    private val initialPoint1 = Geometry.Point2D(x = 0, y = 0)
+    private val destinationOnXAxis = Geometry.Point2D(x = 5, y = 0)
+    private val destinationOnYAxis = Geometry.Point2D(x = 0, y = 5)
+    private val destinationOnXYAxis = Geometry.Point2D(x = 5, y = 5)
+
+    test("Point should not move if already reached destination") {
+      val point = Geometry.Point2D(x = 0, y = 0)
+      val movedPoint = point.moved(toward = point, of = 10)
+      point mustBe movedPoint
+    }
+    test("Distance between points should be valid") {
+      initialPoint1.distanceFrom(point = initialPoint1) mustBe
+        0
+      initialPoint1.distanceFrom(point = destinationOnXAxis) mustBe
+        5
+      initialPoint1.distanceFrom(point = destinationOnYAxis) mustBe
+        5
+      initialPoint1.distanceFrom(point = destinationOnXYAxis) mustBe
+        (5 * math.sqrt(2))
+    }
+    test("Point shouldn't move if no movement set") {
+      initialPoint1.moved(toward = destinationOnXAxis, of = 0) mustBe
+        initialPoint1
+    }
+    test("Point moves on x axis towards destination") {
+      initialPoint1.moved(toward = destinationOnXAxis, of = 3) mustBe
+        Geometry.Point2D(x = 3, y = 0)
+      initialPoint1.moved(toward = destinationOnXAxis, of = 5) mustBe
+        destinationOnXAxis
+      initialPoint1.moved(toward = destinationOnXAxis, of = 10) mustBe
+        Geometry.Point2D(x = 5, y = 0)
+    }
+    test("Point moves on y axis towards destination") {
+      initialPoint1.moved(toward = destinationOnYAxis, of = 3) mustBe
+        Geometry.Point2D(x = 0, y = 3)
+      initialPoint1.moved(toward = destinationOnYAxis, of = 5) mustBe
+        destinationOnYAxis
+      initialPoint1.moved(toward = destinationOnYAxis, of = 10) mustBe
+        destinationOnYAxis
+    }
+    test("Point moves on x and y axes towards destination") {
+      initialPoint1.moved(
+        toward = destinationOnXYAxis,
+        of = 3 * math.sqrt(2)
+      ) mustBe
+        Geometry.Point2D(x = 3, y = 3)
+      initialPoint1.moved(
+        toward = destinationOnXYAxis,
+        of = 5 * math.sqrt(2)
+      ) mustBe
+        destinationOnXYAxis
+      initialPoint1.moved(
+        toward = destinationOnXYAxis,
+        of = 10
+      ) mustBe
+        destinationOnXYAxis
+    }
+  class Polygon2DTest() extends AnyFunSuite with Matchers:
+    private val emptyPolygon = Geometry.Polygon2D(vertexes = List.empty)
+    private val square = Geometry.Polygon2D(vertexes =
+      List(
+        Geometry.Point2D(x = 0, y = 0),
+        Geometry.Point2D(x = 0, y = 5),
+        Geometry.Point2D(x = 5, y = 5),
+        Geometry.Point2D(x = 5, y = 0)
+      )
+    )
+    private val rectangle = Geometry.Polygon2D(vertexes =
+      List(
+        Geometry.Point2D(x = 0, y = 0),
+        Geometry.Point2D(x = 0, y = 10),
+        Geometry.Point2D(x = 5, y = 10),
+        Geometry.Point2D(x = 5, y = 0)
+      )
+    )
+    test("Square contains point") {
+      square.contains(Geometry.Point2D(0, 0)) mustBe true
+      square.contains(Geometry.Point2D(2, 2)) mustBe true
+      square.contains(Geometry.Point2D(5, 5)) mustBe true
+    }
+    test("Square doesn't contain point") {
+      square.contains(Geometry.Point2D(10, 10)) mustBe false
+    }
+
+class GeometryTest
+    extends Suites(
+      new GeometryTest.Point2DTest(),
+      new GeometryTest.Polygon2DTest()
+    )
