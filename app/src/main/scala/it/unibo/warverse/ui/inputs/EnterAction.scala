14cc785de4c9d0268a6ff40026893618461051c6
Nicholas Ricci <nicholas.ricci2@studio.unibo.it>
2022-11-21 17:04:01 +0100

    feat: Created game controller

--- app/src/main/scala/it/unibo/warverse/ui/inputs/EnterAction.scala
+++ app/src/main/scala/it/unibo/warverse/ui/inputs/EnterAction.scala
@@ -13,6 +13,7 @@ import it.unibo.warverse.ui.view.{
 import javax.swing.AbstractAction
 import java.awt.event.ActionEvent
 import javax.swing.JPanel
+import it.unibo.warverse.controller.GameStateController
 
 class EnterAction(
   panel: MenuActions,
@@ -22,10 +23,10 @@ class EnterAction(
 
   override def actionPerformed(e: ActionEvent): Unit =
     panel.getMenuItems match
-      case "Start Game" => setNewPanel(new GamePanel())
-      case "Options"    => setNewPanel(new MenuOptions())
-      case "Help"       => setNewPanel(new MenuHelp(mainFrame))
-      case "Exit"       => System.exit(0)
-      case _            =>
+      case "Start Game" => new GameStateController().setMain(mainFrame)
+      case "Options" => setNewPanel(new MenuOptions())
+      case "Help"    => setNewPanel(new MenuHelp(mainFrame))
+      case "Exit"    => System.exit(0)
+      case _         =>
     panel.repaint()
     panel.invalidate()
