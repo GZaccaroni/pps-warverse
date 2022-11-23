46e5758da8c8615b1a558ba1eff254132fdfc192
alexbaiardi <alex.baiardi99@gmail.com>
2022-11-23 18:31:55 +0100

    refactor: refactor InterstateRelation usage

--- app/src/main/scala/it/unibo/warverse/controller/GameStateController.scala
+++ app/src/main/scala/it/unibo/warverse/controller/GameStateController.scala
@@ -1,14 +1,17 @@
 package it.unibo.warverse.controller
 
+import it.unibo.warverse.model.world.Relations
 import it.unibo.warverse.ui.view.GameMap
 import it.unibo.warverse.ui.view.Hud
 import it.unibo.warverse.ui.view.GamePanel
+
 import java.awt.BorderLayout
 import it.unibo.warverse.ui.view.MainFrame
-import it.unibo.warverse.model.world.InterstateRelations.InterstateRelationsImpl
+import it.unibo.warverse.model.world.Relations.InterstateRelations
 import it.unibo.warverse.model.world.World.Country
-import it.unibo.warverse.model.world.InterstateRelations
-import it.unibo.warverse.model.world.InterstateRelations.Relation
+import it.unibo.warverse.model.world.Relations.Relation
+
+import scala.language.postfixOps
 
 class GameStateController:
   var mainFrame: MainFrame = _
@@ -19,7 +22,7 @@ class GameStateController:
 
   val gamePanel = new GamePanel()
 
-  var interstateRelation: InterstateRelationsImpl = _
+  var interstateRelation: InterstateRelations = _
   var countries: List[Country] = List()
 
   def setMain(mainFrame: MainFrame): Unit =
@@ -45,7 +48,7 @@ class GameStateController:
     allianceList: Map[String, List[String]],
     enemyList: Map[String, List[String]]
   ): Unit =
-    interstateRelation = InterstateRelationsImpl(
+    interstateRelation = InterstateRelations(
       cycleMapRelations(allianceList, false) ++ cycleMapRelations(
         enemyList,
         true
@@ -66,10 +69,9 @@ class GameStateController:
 
         relations = relations ++ List(
           (
-            show(currentCountry),
-            show(selected),
-            if isEnemy then InterstateRelations.RelationStatus.WAR
-            else InterstateRelations.RelationStatus.ALLIANCE
+            (show(currentCountry), show(selected)),
+            if isEnemy then Relations.RelationStatus.WAR
+            else Relations.RelationStatus.ALLIANCE
           )
         )
       )
