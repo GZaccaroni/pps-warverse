69d0fc2f8cbbf5ca552379fcb92cf483e9d074b1
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-24 18:31:38 +0100

    refactor: Updated Attacker trait

--- app/src/main/scala/it/unibo/warverse/model/fight/Fight.scala
+++ app/src/main/scala/it/unibo/warverse/model/fight/Fight.scala
@@ -1,28 +1,21 @@
 package it.unibo.warverse.model.fight
 
-import it.unibo.warverse.model.common.Life
-import it.unibo.warverse.model.common.Math.Percentage
-import it.unibo.warverse.model.common.Movement.Movable
+import it.unibo.warverse.model.common.Movement.Locatable
+import it.unibo.warverse.model.common.{Geometry, Life}
+import it.unibo.warverse.model.fight.SimulationEvent.AttackEvent
+import it.unibo.warverse.model.fight.AttackStrategy
 
 object Fight:
-  trait Attacker[Entity <: Attacker[Entity]]:
-
-    def chanceOfHit: Percentage
-    def rangeOfHit: Double
-    def availableHits: Int
-
+  trait Attacker:
     def attack(
-      availableTargets: List[Attackable[?]]
-    ): List[Attackable[?]]
+      strategy: AttackStrategy
+    ): List[AttackEvent]
   trait Attackable
   trait AttackableUnit extends Attackable, Locatable:
     override type Position = Geometry.Point2D
   trait AttackableArea extends Attackable, Locatable:
     override type Position = Geometry.Polygon2D
 
-  trait AttackerWithAreaImpact[Entity <: Attacker[Entity]]
-      extends Attacker[Entity]:
-    def areaOfImpact: Double
   enum AttackType:
     case Area
     case Precision
