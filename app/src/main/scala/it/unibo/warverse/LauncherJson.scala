cfacb1fa9b33970d612916bc36eff3056f0bcaa7
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-25 13:21:33 +0100

    chore: Added test json launcher

---
+++ app/src/main/scala/it/unibo/warverse/LauncherJson.scala
@@ -0,0 +1,20 @@
+package it.unibo.warverse
+
+import it.unibo.warverse.data.data_sources.json.SimulationConfigDataSource
+import it.unibo.warverse.ui.view.{MainFrame, Menu}
+
+import java.awt.{BorderLayout, Dimension}
+import java.io.File
+import javax.swing.{JFrame, JPanel, SwingUtilities}
+import scala.io.Source
+
+object LauncherJson extends App:
+  val jsonFile = File(
+    "test_v2.json"
+  )
+
+  val jsonConfigParser: SimulationConfigDataSource = SimulationConfigDataSource(
+    jsonFile
+  )
+
+  println(jsonConfigParser.simulationConfig)
