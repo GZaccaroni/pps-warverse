d0f684b959fbbf7226300d22c0415ed1ba21fadf
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-25 13:22:00 +0100

    refactor: Replaced Environment with SimulationConfig

--- app/src/main/scala/it/unibo/warverse/model/SimulationConfig.scala
+++ app/src/main/scala/it/unibo/warverse/model/SimulationConfig.scala
@@ -3,10 +3,4 @@ package it.unibo.warverse.model
 import it.unibo.warverse.model.world.World.Country
 import it.unibo.warverse.model.fight.Army.ArmyUnit
 
-trait Enviroment:
-  def countries: List[Country]
-  def army: List[ArmyUnit]
-  def day: Integer
-
-class EnviromentImpl:
-  println()
+case class SimulationConfig(countries: List[Country])
