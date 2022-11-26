e8606c1a1af9d0987127df0da8f41b0805a5a3b6
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-26 12:16:06 +0100

    feat: Implemented country validation

--- app/src/main/scala/it/unibo/warverse/data/models/WorldDtos.scala
+++ app/src/main/scala/it/unibo/warverse/data/models/WorldDtos.scala
@@ -3,6 +3,7 @@ package it.unibo.warverse.data.models
 import it.unibo.warverse.data.models.GeometryDtos
 import it.unibo.warverse.data.models.ArmyDtos
 import it.unibo.warverse.domain.model.world.World
+import it.unibo.warverse.domain.model.common.Validation.*
 
 object WorldDtos:
   case class CountryDto(
@@ -12,7 +13,13 @@ object WorldDtos:
     citizens: Int,
     relations: CountryRelationsDto = CountryRelationsDto(),
     army: ArmyDtos.CountryArmy
-  )
+  ) extends Validatable:
+    override def validate(): Unit =
+      given ValidatedEntity = ValidatedEntity(this.getClass.getTypeName)
+      if resources < 0 then throw "resources" isNotGreaterOrEqualThan 0
+      if boundaries.length < 3 then throw "boundaries" isNotGreaterOrEqualThan 3
+      if citizens < 0 then throw "citizens" isNotGreaterOrEqualThan 0
+      army.validate()
 
   case class CountryRelationsDto(
     allies: List[World.CountryId] = List.empty,
