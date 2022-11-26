01609bb6c67295be523d05ee0a4dcceef9969686
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-26 12:16:14 +0100

    feat: Implemented simulation config validation

--- app/src/main/scala/it/unibo/warverse/data/models/SimulationConfigDtos.scala
+++ app/src/main/scala/it/unibo/warverse/data/models/SimulationConfigDtos.scala
@@ -1,9 +1,29 @@
 package it.unibo.warverse.data.models
 
 import it.unibo.warverse.data.models.WorldDtos
+import it.unibo.warverse.domain.model.common.Validation.*
 import it.unibo.warverse.domain.model.world.World
 
 object SimulationConfigDtos:
   case class SimulationConfigDto(
     countries: List[WorldDtos.CountryDto]
-  )
+  ) extends Validatable:
+    override def validate(): Unit =
+      given ValidatedEntity = ValidatedEntity(this.getClass.getTypeName)
+      countries.foreach(country =>
+        country.validate()
+        val relationships =
+          country.relations.enemies ++ country.relations.allies
+        if relationships.contains(country.id) then
+          throw ValidationException(
+            s"Country ${country.id} can't be allied or in war with himself"
+          )
+        if relationships.exists(relId => !countries.exists(relId == _.id)) then
+          throw ValidationException(
+            s"Not all relationships have countries defined"
+          )
+      )
+      if countries.map(_.id).toSet.size != countries.size then
+        throw ValidationException(
+          s"Some countries have the same id"
+        )
