fc78d4e333a9c679a9f144ca8d7caa69890cfead
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-11-26 12:15:57 +0100

    feat: Implemented point2d validation

--- app/src/main/scala/it/unibo/warverse/data/models/GeometryDtos.scala
+++ app/src/main/scala/it/unibo/warverse/data/models/GeometryDtos.scala
@@ -1,4 +1,10 @@
 package it.unibo.warverse.data.models
 
+import it.unibo.warverse.domain.model.common.Validation.*
+
 object GeometryDtos:
-  case class Point2DDto(x: Double, y: Double)
+  case class Point2DDto(x: Double, y: Double) extends Validatable:
+    override def validate(): Unit =
+      given ValidatedEntity = ValidatedEntity(this.getClass.getTypeName)
+      if x < 0 then throw "x" isNotGreaterOrEqualThan 0
+      if y < 0 then throw "y" isNotGreaterOrEqualThan 0
