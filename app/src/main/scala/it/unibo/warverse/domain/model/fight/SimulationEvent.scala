73afdcb48adbd6d09eb29979af1a9484118ebf62
alexbaiardi <alex.baiardi99@gmail.com>
2022-11-30 11:38:58 +0100

    feat: defined different type of AttackEvent

--- app/src/main/scala/it/unibo/warverse/domain/model/fight/SimulationEvent.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/fight/SimulationEvent.scala
@@ -1,6 +1,13 @@
 package it.unibo.warverse.domain.model.fight
 
+import it.unibo.warverse.domain.model.common.Geometry.Point2D
+
 object SimulationEvent:
   sealed abstract class SimulationEvent
 
-  class AttackEvent(target: Fight.Attackable)
+  trait AttackEvent
+
+  case class AreaAttackEvent(target: Point2D, areaOfImpact: Double)
+      extends AttackEvent
+
+  case class PrecisionAttackEvent(target: Point2D) extends AttackEvent
