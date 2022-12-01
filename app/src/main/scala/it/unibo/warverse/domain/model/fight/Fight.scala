a2a4e24f1ee3e70e297bceb57f38516b828ab05d
alexbaiardi <alex.baiardi99@gmail.com>
2022-12-01 10:28:09 +0100

    feat: updated Attacker.attack function params

--- app/src/main/scala/it/unibo/warverse/domain/model/fight/Fight.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/fight/Fight.scala
@@ -3,12 +3,12 @@ package it.unibo.warverse.domain.model.fight
 import it.unibo.warverse.domain.model.common.Movement.Locatable
 import it.unibo.warverse.domain.model.common.{Geometry, Life}
 import it.unibo.warverse.domain.model.fight.SimulationEvent.AttackEvent
-import it.unibo.warverse.domain.model.fight.AttackStrategy
+import it.unibo.warverse.domain.model.fight.AttackStrategy.AttackStrategy2D
 
 object Fight:
   trait Attacker:
     def attack(
-      strategy: AttackStrategy
+      strategy: AttackStrategy2D
     ): List[AttackEvent]
   trait Attackable
   trait AttackableUnit extends Attackable, Locatable:
