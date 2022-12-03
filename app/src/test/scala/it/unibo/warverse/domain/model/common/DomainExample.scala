947af30697ced1f8fb5eee118c1ad2446c84cbd5
alexbaiardi <alex.baiardi99@gmail.com>
2022-12-03 11:45:19 +0100

    feat: enriched examples for test

--- app/src/test/scala/it/unibo/warverse/domain/model/common/DomainExample.scala
+++ app/src/test/scala/it/unibo/warverse/domain/model/common/DomainExample.scala
@@ -1,12 +1,52 @@
 package it.unibo.warverse.domain.model.common
 
-import it.unibo.warverse.domain.model.common.Geometry.Polygon2D
+import it.unibo.warverse.domain.model.Environment
+import it.unibo.warverse.domain.model.common.Geometry.{Point2D, Polygon2D}
+import it.unibo.warverse.domain.model.fight.Army.AreaArmyUnit
+import it.unibo.warverse.domain.model.world.Relations.{
+  InterstateRelations,
+  RelationStatus
+}
 import it.unibo.warverse.domain.model.world.World.Country
 
 object DomainExample:
   val countryA: Country =
-    Country("ID_1", "A", 40, List.empty, 0.0, Polygon2D(List.empty))
+    Country(
+      "ID_1",
+      "A",
+      40,
+      Seq.empty,
+      0.0,
+      Polygon2D(Seq(Point2D(0, 0), Point2D(0, 3), Point2D(3, 3), Point2D(3, 0)))
+    )
   val countryB: Country =
-    Country("ID_2", "B", 10, List.empty, 0.0, Polygon2D(List.empty))
+    Country(
+      "ID_2",
+      "B",
+      10,
+      Seq.empty,
+      0.0,
+      Polygon2D(Seq(Point2D(3, 0), Point2D(3, 3), Point2D(6, 3), Point2D(6, 0)))
+    )
   val countryC: Country =
-    Country("ID_3", "C", 20, List.empty, 0.0, Polygon2D(List.empty))
+    Country(
+      "ID_3",
+      "C",
+      20,
+      Seq.empty,
+      0.0,
+      Polygon2D(Seq(Point2D(6, 0), Point2D(6, 3), Point2D(9, 3), Point2D(9, 0)))
+    )
+
+  val interstateRelations: InterstateRelations = InterstateRelations(
+    Seq(
+      ((countryA.id, countryB.id), RelationStatus.WAR),
+      ((countryB.id, countryC.id), RelationStatus.ALLIANCE)
+    )
+  )
+
+  val environment: Environment = Environment(
+    Seq(countryA, countryB, countryC),
+    interstateRelations,
+    0
+  )
