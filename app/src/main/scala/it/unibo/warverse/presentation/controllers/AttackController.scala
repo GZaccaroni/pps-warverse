9de489daf611cec84bbc507d3bc0675346fc49bc
alexbaiardi <alex.baiardi99@gmail.com>
2022-12-03 18:13:28 +0100

    refactor: defined trait AttackController and companion object

--- app/src/main/scala/it/unibo/warverse/presentation/controllers/AttackController.scala
+++ app/src/main/scala/it/unibo/warverse/presentation/controllers/AttackController.scala
@@ -10,37 +10,46 @@ import it.unibo.warverse.domain.model.fight.SimulationEvent.{
 }
 import it.unibo.warverse.domain.model.world.World.Country
 
-class AttackController:
+trait AttackController:
+  def attackAndUpdate(environment: Environment): Environment
 
-  def attackAndUpdate(environment: Environment): Environment =
-    val events = for
-      country <- environment.countries
-      if environment.interstateRelations.countryEnemies(country.id).nonEmpty
-      unit <- country.armyUnits
-      event <- unit.attack(
-        AttackStrategy.attackStrategy2D(environment, unit.countryId)
+object AttackController:
+  def apply(environment: Environment): AttackController =
+    AttackControllerImpl(environment)
+
+  private case class AttackControllerImpl(environment: Environment)
+      extends AttackController:
+
+    def attackAndUpdate(environment: Environment): Environment =
+      val events = for
+        country <- environment.countries
+        if environment.interstateRelations.countryEnemies(country.id).nonEmpty
+        unit <- country.armyUnits
+        event <- unit.attack(
+          AttackStrategy.attackStrategy2D(environment, unit.countryId)
+        )
+      yield event
+      updateEvent(environment, events)
+
+    def updateEvent(
+      environment: Environment,
+      attackEvents: Seq[AttackEvent]
+    ): Environment =
+      val areaAttackEvents = attackEvents.collect({
+        case event: AreaAttackEvent =>
+          event
+      })
+      val updatedCountries: Iterable[(Country, Country)] = for
+        event <- areaAttackEvents
+        target = event.target
+        areaOfImpact = event.areaOfImpact
+        country <- environment.countries if country.boundaries.center == target
+        density = country.citizens / country.boundaries.area
+        citizens = country.citizens - (density * areaOfImpact).toInt
+        newCountry = country.copy(citizens = citizens)
+      yield (country, newCountry)
+      environment.copiedWith(countries =
+        environment.countries
+          .diff(updatedCountries.map(_._1).toSeq)
+          .concat(updatedCountries.map(_._2))
       )
-    yield event
-    updateEvent(environment, events)
-
-  def updateEvent(
-    environment: Environment,
-    attackEvents: Seq[AttackEvent]
-  ): Environment =
-    val areaAttackEvents = attackEvents.collect({ case event: AreaAttackEvent =>
-      event
-    })
-    val updatedCountries: Iterable[(Country, Country)] = for
-      event <- areaAttackEvents
-      target = event.target
-      areaOfImpact = event.areaOfImpact
-      country <- environment.countries if country.boundaries.center == target
-      density = country.citizens / country.boundaries.area
-      citizens = country.citizens - (density * areaOfImpact).toInt
-      newCountry = country.copy(citizens = citizens)
-    yield (country, newCountry)
-    environment.copiedWith(countries =
-      environment.countries
-        .diff(updatedCountries.map(_._1).toSeq)
-        .concat(updatedCountries.map(_._2))
-    )
