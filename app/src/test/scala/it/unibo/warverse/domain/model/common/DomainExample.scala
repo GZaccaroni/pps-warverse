d7b719a78e2fb1c9ada010312143e3b7eb7ca3b7
alexbaiardi <alex.baiardi99@gmail.com>
2022-12-03 12:31:36 +0100

    feat: add soldiers example

--- app/src/test/scala/it/unibo/warverse/domain/model/common/DomainExample.scala
+++ app/src/test/scala/it/unibo/warverse/domain/model/common/DomainExample.scala
@@ -10,12 +10,48 @@ import it.unibo.warverse.domain.model.world.Relations.{
 import it.unibo.warverse.domain.model.world.World.Country
 
 object DomainExample:
+  val successfullySoldier: AreaArmyUnit =
+    AreaArmyUnit(
+      "ID_A",
+      "soldier",
+      Point2D(3, 1.5),
+      chanceOfHit = 100,
+      rangeOfHit = 2,
+      availableHits = 1,
+      areaOfImpact = 0.5,
+      dailyConsume = 100,
+      speed = 0.1
+    )
+  val failingSoldier: AreaArmyUnit =
+    AreaArmyUnit(
+      "ID_A",
+      "soldier",
+      Point2D(4, 1.5),
+      chanceOfHit = 0,
+      rangeOfHit = 1,
+      availableHits = 3,
+      areaOfImpact = 0.5,
+      dailyConsume = 1,
+      speed = 0.2
+    )
+  val distantSoldier: AreaArmyUnit =
+    AreaArmyUnit(
+      "ID_A",
+      "soldier",
+      Point2D(1.5, 1.5),
+      chanceOfHit = 100,
+      rangeOfHit = 1,
+      availableHits = 3,
+      areaOfImpact = 0.5,
+      dailyConsume = 1,
+      speed = 0.05
+    )
   val countryA: Country =
     Country(
       "ID_1",
       "A",
       40,
-      Seq.empty,
+      Seq(successfullySoldier, failingSoldier, distantSoldier),
       0.0,
       Polygon2D(Seq(Point2D(0, 0), Point2D(0, 3), Point2D(3, 3), Point2D(3, 0)))
     )
