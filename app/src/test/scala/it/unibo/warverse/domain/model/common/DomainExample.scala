8f5459b5fb4e6794b9d0fd29a10802c03962546d
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-05 13:13:07 +0100

    test: Separated domain example objects

--- app/src/test/scala/it/unibo/warverse/domain/model/common/DomainExample.scala
+++ app/src/test/scala/it/unibo/warverse/domain/model/common/DomainExample.scala
@@ -2,71 +2,124 @@ package it.unibo.warverse.domain.model.common
 
 import it.unibo.warverse.domain.model.Environment
 import it.unibo.warverse.domain.model.common.Geometry.{Point2D, Polygon2D}
-import it.unibo.warverse.domain.model.fight.Army.AreaArmyUnit
+import it.unibo.warverse.domain.model.fight.Army.{
+  AreaArmyUnit,
+  PrecisionArmyUnit
+}
 import it.unibo.warverse.domain.model.world.Relations.{
   InterstateRelations,
   RelationStatus
 }
 import it.unibo.warverse.domain.model.world.World.Country
 
+import java.util.UUID
+
 object DomainExample:
-  val successfullySoldier: AreaArmyUnit =
-    AreaArmyUnit(
-      "ID_A",
-      "soldier",
-      Point2D(3, 1.5),
-      chanceOfHit = 100,
-      rangeOfHit = 2,
-      availableHits = 1,
-      areaOfImpact = 0.5,
-      dailyConsume = 100,
-      speed = 0.1
-    )
-  val failingSoldier: AreaArmyUnit =
-    AreaArmyUnit(
-      "ID_A",
-      "soldier",
-      Point2D(4, 1.5),
-      chanceOfHit = 0,
-      rangeOfHit = 1,
-      availableHits = 3,
-      areaOfImpact = 0.5,
-      dailyConsume = 1,
-      speed = 0.2
-    )
-  val distantSoldier: AreaArmyUnit =
-    AreaArmyUnit(
-      "ID_A",
-      "soldier",
-      Point2D(1.5, 1.5),
-      chanceOfHit = 100,
-      rangeOfHit = 1,
-      availableHits = 3,
-      areaOfImpact = 0.5,
-      dailyConsume = 1,
-      speed = 0.05
-    )
+
+  val countryAId = "ID_A"
+  val countryBId = "ID_B"
+  val countryCId = "ID_C"
+  object Army:
+    object AreaArmyUnits:
+      val successfulUnit: AreaArmyUnit =
+        AreaArmyUnit(
+          countryAId,
+          "catapult",
+          Point2D(3, 1.5),
+          chanceOfHit = 100,
+          rangeOfHit = 2,
+          availableHits = 1,
+          areaOfImpact = 0.5,
+          dailyConsume = 100,
+          speed = 0.1
+        )
+      val failingUnit: AreaArmyUnit =
+        AreaArmyUnit(
+          countryAId,
+          "catapult",
+          Point2D(4, 1.5),
+          chanceOfHit = 0,
+          rangeOfHit = 1,
+          availableHits = 3,
+          areaOfImpact = 0.5,
+          dailyConsume = 1,
+          speed = 0.2
+        )
+      val distantUnit: AreaArmyUnit =
+        AreaArmyUnit(
+          countryAId,
+          "catapult",
+          Point2D(1.5, 1.5),
+          chanceOfHit = 100,
+          rangeOfHit = 1,
+          availableHits = 3,
+          areaOfImpact = 0.5,
+          dailyConsume = 1,
+          speed = 0.05
+        )
+    object PrecisionArmyUnits:
+      val successfulUnit =
+        PrecisionArmyUnit(
+          countryBId,
+          "soldier",
+          Point2D(3.5, 1.5),
+          chanceOfHit = 100,
+          rangeOfHit = 2,
+          availableHits = 1,
+          dailyConsume = 100,
+          speed = 0.1
+        )
+      val failingUnit =
+        PrecisionArmyUnit(
+          countryBId,
+          "soldier",
+          Point2D(3.5, 1.5),
+          chanceOfHit = 0,
+          rangeOfHit = 1,
+          availableHits = 3,
+          dailyConsume = 1,
+          speed = 0.2
+        )
+      val distantUnit =
+        PrecisionArmyUnit(
+          countryBId,
+          "soldier",
+          Point2D(8, 8),
+          chanceOfHit = 100,
+          rangeOfHit = 1,
+          availableHits = 3,
+          dailyConsume = 1,
+          speed = 0.05
+        )
   val countryA: Country =
     Country(
-      "ID_1",
+      countryAId,
       "A",
       40,
-      Seq(successfullySoldier, failingSoldier, distantSoldier),
+      Seq(
+        Army.AreaArmyUnits.successfulUnit,
+        Army.AreaArmyUnits.failingUnit,
+        Army.AreaArmyUnits.distantUnit
+      ),
       0.0,
       Polygon2D(Seq(Point2D(0, 0), Point2D(0, 3), Point2D(3, 3), Point2D(3, 0)))
     )
   val countryB: Country =
     Country(
-      "ID_2",
+      countryBId,
       "B",
       10,
-      Seq.empty,
+      Seq(
+        Army.PrecisionArmyUnits.successfulUnit,
+        Army.PrecisionArmyUnits.failingUnit,
+        Army.PrecisionArmyUnits.distantUnit
+      ),
       0.0,
       Polygon2D(Seq(Point2D(3, 0), Point2D(3, 3), Point2D(6, 3), Point2D(6, 0)))
     )
   val countryC: Country =
     Country(
-      "ID_3",
+      countryCId,
       "C",
       20,
       Seq.empty,
