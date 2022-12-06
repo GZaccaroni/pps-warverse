b95e934ff75e60d5dde6f41a8a32b2b347de9b0c
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-06 13:20:06 +0100

    feat: Added Simulation Events (Iteration Completed, Simulation Completed, War Win)

--- app/src/main/scala/it/unibo/warverse/domain/model/fight/SimulationEvent.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/fight/SimulationEvent.scala
@@ -1,25 +1,11 @@
 package it.unibo.warverse.domain.model.fight
 
-import it.unibo.warverse.domain.model.common.Geometry.Point2D
+import it.unibo.warverse.domain.model.Environment
+import it.unibo.warverse.domain.model.world.World.CountryId
 
+sealed trait SimulationEvent
 object SimulationEvent:
-  sealed abstract class SimulationEvent
 
-  /** Represent an attack event
-    */
-  trait AttackEvent
-
-  /** Case class that represent an AttackEvent on a specified area
-    * @param target
-    *   the position of the attack
-    * @param areaOfImpact
-    *   the area involved in the attack
-    */
-  case class AreaAttackEvent(target: Point2D, areaOfImpact: Double)
-      extends AttackEvent
-
-  /** Case class that represent an AttackEvent on a target unit
-    * @param target
-    *   the position of the attack
-    */
-  case class PrecisionAttackEvent(target: Point2D) extends AttackEvent
+  case class IterationCompleted(environment: Environment) extends SimulationEvent
+  object SimulationCompletedEvent extends SimulationEvent
+  case class CountryWinWarEvent(winnerId: CountryId, loserId: CountryId, day: Int) extends SimulationEvent
