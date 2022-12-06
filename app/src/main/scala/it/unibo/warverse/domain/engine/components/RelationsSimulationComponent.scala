29882026ebe827a0c97330e042547d9f23629b43
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-06 13:22:03 +0100

    feat: Added RelationsSimulationComponent

---
+++ app/src/main/scala/it/unibo/warverse/domain/engine/components/RelationsSimulationComponent.scala
@@ -0,0 +1,30 @@
+package it.unibo.warverse.domain.engine.components
+
+import it.unibo.warverse.domain.model.Environment
+import it.unibo.warverse.domain.model.world.Relations.*
+import it.unibo.warverse.domain.model.world.World.Country
+
+class RelationsSimulationComponent extends SimulationComponent:
+  override def run(environment: Environment): Environment =
+    var newRel = environment.interstateRelations
+    environment.countries.foreach(country =>
+      val enemies = environment.interstateRelations.countryEnemies(country.id)
+      val allied = environment.interstateRelations.countryAllies(country.id)
+      enemies.map(enemy =>
+        allied.foreach(ally =>
+          if newRel.getStatus(enemy, ally).isEmpty then
+            newRel = newRel.withRelation((enemy, ally), RelationStatus.WAR)
+        )
+      )
+    )
+    environment.copiedWith(interstateRelations = newRel)
+
+  def noWars(
+    relations: InterstateRelations,
+    countries: Seq[Country]
+  ): Boolean =
+    var check = true
+    countries.foreach(country =>
+      if relations.countryEnemies(country.id).nonEmpty then check = false
+    )
+    check
