12c209578c04d36d92106d393213263c078feb0a
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-06 13:27:59 +0100

    refactor: Renamed update methods in Country

--- app/src/main/scala/it/unibo/warverse/domain/model/world/World.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/world/World.scala
@@ -1,7 +1,6 @@
 package it.unibo.warverse.domain.model.world
 
 import it.unibo.warverse.domain.model.common.Life
-import it.unibo.warverse.domain.model.fight.Fight.Attackable
 import it.unibo.warverse.domain.model.common.{Geometry, Life, Movement}
 import it.unibo.warverse.domain.model.fight.Army.ArmyUnit
 import it.unibo.warverse.domain.model.fight.Fight
@@ -10,9 +9,9 @@ object World:
   type CountryId = String
 
   trait UpdateResources:
-    def updateResources(newResources: Life.Resources): Country
-    def updateCitizen(newCitizen: Int): Country
-    def updateArmy(newArmy: Seq[ArmyUnit]): Country
+    def addingResources(newResources: Life.Resources): Country
+    def addingCitizens(newCitizen: Int): Country
+    def addingArmyUnits(newArmy: Seq[ArmyUnit]): Country
 
   case class Country(
     id: CountryId,
@@ -22,13 +21,13 @@ object World:
     resources: Life.Resources,
     boundaries: Geometry.Polygon2D
   ) extends UpdateResources:
-    override def updateResources(newResources: Life.Resources): Country =
+    override def addingResources(newResources: Life.Resources): Country =
       if resources + newResources < 0 then this.copy(resources = 0)
       else this.copy(resources = resources + newResources)
 
-    override def updateCitizen(newCitizen: Int): Country =
+    override def addingCitizens(newCitizen: Int): Country =
       if citizens + newCitizen < 0 then this.copy(citizens = 0)
       this.copy(citizens = citizens + newCitizen)
 
-    override def updateArmy(newArmy: Seq[ArmyUnit]): Country =
+    override def addingArmyUnits(newArmy: Seq[ArmyUnit]): Country =
       this.copy(armyUnits = armyUnits.concat(newArmy))
