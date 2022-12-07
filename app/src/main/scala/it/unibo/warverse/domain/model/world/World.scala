1d7c9eb44de6e10bc46a57445caaa9d91f6e8750
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-07 13:15:24 +0100

    docs: Added documentation to Country

--- app/src/main/scala/it/unibo/warverse/domain/model/world/World.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/world/World.scala
@@ -20,14 +20,32 @@ object World:
     armyUnits: Seq[ArmyUnit],
     resources: Life.Resources,
     boundaries: Geometry.Polygon2D
-  ) extends UpdateResources:
+  ) extends UpdatableAssets[Country]:
+    /** It returns a new {@link Country} with the given resources added
+      * @param newResources
+      *   the resources to be added
+      * @return
+      *   a new instance of {@link Country} with the resources added
+      */
     override def addingResources(newResources: Life.Resources): Country =
       if resources + newResources < 0 then this.copy(resources = 0)
       else this.copy(resources = resources + newResources)
 
-    override def addingCitizens(newCitizen: Int): Country =
+    /** It returns a new {@link Country} with the given citizens added
+      * @param newCitizens
+      *   the citizens to be added
+      * @return
+      *   a new instance of {@link Country} with the citizens added
+      */
+    override def addingCitizens(newCitizens: Int): Country =
       if citizens + newCitizen < 0 then this.copy(citizens = 0)
-      this.copy(citizens = citizens + newCitizen)
+      this.copy(citizens = citizens + newCitizens)
 
-    override def addingArmyUnits(newArmy: Seq[ArmyUnit]): Country =
-      this.copy(armyUnits = armyUnits.concat(newArmy))
+    /** * It returns a new {@link Country} with the given army units added
+      * @param newArmyUnits
+      *   the army units to be added
+      * @return
+      *   a new instance of {@link Country} with the army units added
+      */
+    override def addingArmyUnits(newArmyUnits: Seq[ArmyUnit]): Country =
+      this.copy(armyUnits = newArmyUnits.concat(newArmy))
