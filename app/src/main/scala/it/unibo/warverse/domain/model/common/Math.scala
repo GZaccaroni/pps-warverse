23f5f954a4a6e9588848ef7dc3567571ddae6e9e
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-07 13:17:43 +0100

    docs: Documented almostEquals method

--- app/src/main/scala/it/unibo/warverse/domain/model/common/Math.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/common/Math.scala
@@ -1,7 +1,23 @@
 package it.unibo.warverse.domain.model.common
 
+import scala.annotation.targetName
+
 object Math:
   type Percentage = Double
+
+  /** Double comparison precision
+    * @param p
+    *   actual precision value
+    */
   case class Precision(p: Double)
   implicit class DoubleWithAlmostEquals(val d: Double) extends AnyVal:
+    /** Compares two value given a precision
+      * @param d2
+      *   the second value to compare
+      * @param p
+      *   the precision to use
+      * @return
+      *   if the two value are the same taking precision into account
+      */
+    @targetName("almostEquals")
     def ~=(d2: Double)(using p: Precision): Boolean = (d - d2).abs < p.p
