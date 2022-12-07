acfdbdd6714be94217318e8c6dd492d4edc2664e
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-07 13:14:56 +0100

    docs: Added documentation to Environment

--- app/src/main/scala/it/unibo/warverse/domain/model/Environment.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/Environment.scala
@@ -2,20 +2,56 @@ package it.unibo.warverse.domain.model
 
 import it.unibo.warverse.domain.model.world.World.Country
 import it.unibo.warverse.domain.model.fight.Army.ArmyUnit
-import it.unibo.warverse.domain.model.world.Relations.InterstateRelations
+import it.unibo.warverse.domain.model.world.Relations.InterCountryRelations
 
 import scala.::
 
+/** Environment of the simulation
+  */
 trait Environment:
+  /** Returns the countries
+    * @return
+    *   a sequence of countries
+    */
   def countries: Seq[Country]
-  def interstateRelations: InterstateRelations
+
+  /** Returns the relations between countries
+    *
+    * @return
+    *   relations between countries
+    */
+  def interstateRelations: InterCountryRelations
+
+  /** Returns the current day in the environment
+    *
+    * @return
+    *   current day
+    */
   def day: Int
+
+  /** Creates a copy of environment with the passed parameters updated
+    * @param countries
+    *   new countries
+    * @param interstateRelations
+    *   new interstate releations
+    * @param day
+    *   new day
+    * @return
+    *   a copy of environment
+    */
   def copiedWith(
-    countries: Seq[Country] = countries,
-    interstateRelations: InterstateRelations = interstateRelations,
-    day: Int = day
+                  countries: Seq[Country] = countries,
+                  interstateRelations: InterCountryRelations = interstateRelations,
+                  day: Int = day
   ): Environment =
     Environment(countries, interstateRelations, day)
+
+  /** A new environment with `newCountry` replaced
+    * @param newCountry
+    *   the country to be replaced
+    * @return
+    *   the updated environment
+    */
   def replacingCountry(newCountry: Country): Environment =
     copiedWith(
       newCountry :: countries.filter(_.id != newCountry.id).toList
@@ -23,14 +59,14 @@ trait Environment:
 
 object Environment:
   def apply(
-    countries: Seq[Country],
-    interstateRelations: InterstateRelations,
-    day: Int = 0
+             countries: Seq[Country],
+             interstateRelations: InterCountryRelations,
+             day: Int = 0
   ): Environment =
     EnvironmentImpl(countries, interstateRelations, day)
 
   private case class EnvironmentImpl(
-    override val countries: Seq[Country],
-    override val interstateRelations: InterstateRelations,
-    override val day: Int
+                                      override val countries: Seq[Country],
+                                      override val interstateRelations: InterCountryRelations,
+                                      override val day: Int
   ) extends Environment
--- app/src/main/scala/it/unibo/warverse/presentation/view/MenuOptions.scala
+++ app/src/main/scala/it/unibo/warverse/presentation/view/MenuOptions.scala
@@ -12,9 +12,6 @@ import java.awt.event.ActionEvent
 import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import java.util.ArrayList
-import java.util.List
-import java.util.Map
 import javax.swing.AbstractAction
 import javax.swing.ActionMap
 import javax.swing.InputMap
@@ -26,7 +23,6 @@ import javax.swing.UnsupportedLookAndFeelException
 import javax.swing.SwingUtilities
 import javax.swing.WindowConstants.EXIT_ON_CLOSE
 import javax.swing.JComponent
-import java.util.HashMap
 import javax.swing.JFrame
 import java.awt.BorderLayout
 import java.awt.geom.RoundRectangle2D
