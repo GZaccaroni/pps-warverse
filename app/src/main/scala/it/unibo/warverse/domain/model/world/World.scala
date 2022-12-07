59e8d70a93b78b3f6896cd4dbfa75a1f3a59df6f
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-07 13:15:33 +0100

    docs: Added documentation to UpdatableAssets

--- app/src/main/scala/it/unibo/warverse/domain/model/world/World.scala
+++ app/src/main/scala/it/unibo/warverse/domain/model/world/World.scala
@@ -8,11 +8,47 @@ import it.unibo.warverse.domain.model.fight.Fight
 object World:
   type CountryId = String
 
-  trait UpdateResources:
-    def addingResources(newResources: Life.Resources): Country
-    def addingCitizens(newCitizen: Int): Country
-    def addingArmyUnits(newArmy: Seq[ArmyUnit]): Country
+  trait UpdatableAssets[UpdatableType <: UpdatableAssets[UpdatableType]]:
+    /** It returns a new entity with the given resources added
+      * @param newResources
+      *   the resources to be added
+      * @return
+      *   a new instance of {@link UpdatableType} with the resources added
+      */
+    def addingResources(newResources: Life.Resources): UpdatableType
 
+    /** It returns a new entity with the given citizens added
+      *
+      * @param newCitizens
+      *   the citizens to be added
+      * @return
+      *   a new instance of {@link UpdatableType} with the citizens added
+      */
+    def addingCitizens(newCitizens: Int): UpdatableType
+
+    /** It returns a new entity with the given army units added
+      *
+      * @param newArmyUnits
+      *   the army units to be added
+      * @return
+      *   a new instance of {@link UpdatableType} with the army units added
+      */
+    def addingArmyUnits(newArmyUnits: Seq[ArmyUnit]): UpdatableType
+
+  /** A country
+    * @param id
+    *   The identifier of the country
+    * @param name
+    *   The name of the country
+    * @param citizens
+    *   The number of citizens in the country
+    * @param armyUnits
+    *   The army units in the country
+    * @param resources
+    *   The quantity of resources of the country
+    * @param boundaries
+    *   The boundaries of the country
+    */
   case class Country(
     id: CountryId,
     name: String,
