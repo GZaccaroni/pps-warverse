d53f322acf8e904172a9e493ae5141f2a8867d0d
Dallas <71103219+LeleDallas@users.noreply.github.com>
2022-12-07 14:23:39 +0100

    refactor: moved all in trait to use apply method

--- app/src/main/scala/it/unibo/warverse/presentation/view/MenuHelp.scala
+++ app/src/main/scala/it/unibo/warverse/presentation/view/MenuHelp.scala
@@ -12,66 +12,90 @@ import java.awt.Insets
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 
-class MenuHelp(mainFrame: MainFrame) extends JPanel:
-  this.requestFocus()
-  val startButton = JButton("Start")
-  val closeButton = JButton("Exit App")
-  val generateJsonButton = JButton("Generate Json")
-  val r1 = new JRadioButton("A) Structure");
-  val r2 = new JRadioButton("B) Country");
-  val r3 = new JRadioButton("C) Unit Kind");
-  val r4 = new JRadioButton("D) Units");
-  val r5 = new JRadioButton("E) Boundaries");
-  val r6 = new JRadioButton("F) Relations");
-  val bg = new ButtonGroup()
-  private val console: JTextArea = JTextArea(25, 25)
-  this.console.setMargin(Insets(10, 10, 10, 10))
-  this.console.setEditable(false)
-  this.console.setLineWrap(true)
-  this.console.setWrapStyleWord(true)
-  bg.add(r1)
-  bg.add(r2)
-  bg.add(r3)
-  bg.add(r4)
-  bg.add(r5)
-  bg.add(r6)
-  this.addComponentsToPanel(
-    List(
-      startButton,
-      closeButton,
-      r1,
-      r2,
-      r3,
-      r4,
-      r5,
-      r6,
-      generateJsonButton,
-      console
+trait MenuHelp extends JPanel:
+  def addComponentsToPanel(components: Seq[JComponent]): Unit
+  def addComponentsToButtonGroup(radioButtons: Seq[JRadioButton]): Unit
+  def printInTextField: Unit
+  def jsonPrettyFormatted(stringJson: String): String
+  def mainFrame: MainFrame
+
+object MenuHelp:
+  def apply(mainFrame: MainFrame): MenuHelp = MenuHelpImpl(mainFrame)
+
+  private class MenuHelpImpl(
+    override val mainFrame: MainFrame
+  ) extends MenuHelp:
+    this.requestFocus()
+    val startButton = JButton("Start")
+    val closeButton = JButton("Exit App")
+    val generateJsonButton = JButton("Generate Json")
+    val rdbStructure = new JRadioButton("A) Structure");
+    val rdbCountry = new JRadioButton("B) Country");
+    val rdbUnitKind = new JRadioButton("C) Unit Kind");
+    val rdbUnits = new JRadioButton("D) Units");
+    val rdbBoundaries = new JRadioButton("E) Boundaries");
+    val rdbRelations = new JRadioButton("F) Relations");
+    val buttonGroup = new ButtonGroup()
+    private val console: JTextArea = JTextArea(25, 25)
+    this.console.setMargin(Insets(10, 10, 10, 10))
+    this.console.setEditable(false)
+    this.console.setLineWrap(true)
+    this.console.setWrapStyleWord(true)
+    this.addComponentsToButtonGroup(
+      Seq(
+        rdbStructure,
+        rdbCountry,
+        rdbUnitKind,
+        rdbUnits,
+        rdbBoundaries,
+        rdbRelations
+      )
     )
-  )
-  startButton.addActionListener(_ => GameStateController(mainFrame).setPanel())
-  closeButton.addActionListener(_ => System.exit(0))
-  generateJsonButton.addActionListener((_ => printInTextField))
+    this.addComponentsToPanel(
+      Seq(
+        startButton,
+        closeButton,
+        rdbStructure,
+        rdbCountry,
+        rdbUnitKind,
+        rdbUnits,
+        rdbBoundaries,
+        rdbRelations,
+        generateJsonButton,
+        console
+      )
+    )
+    startButton.addActionListener(_ =>
+      GameStateController(mainFrame).setPanel()
+    )
+    closeButton.addActionListener(_ => System.exit(0))
+    generateJsonButton.addActionListener((_ => printInTextField))
 
-  def jsonPrettyFormatted(stringJson: String): String =
-    val mapper = new ObjectMapper
-    mapper.enable(SerializationFeature.INDENT_OUTPUT)
-    val jsonObject = mapper.readValue(stringJson, classOf[Object])
-    mapper.writeValueAsString(jsonObject)
+    override def jsonPrettyFormatted(stringJson: String): String =
+      val mapper = new ObjectMapper
+      mapper.enable(SerializationFeature.INDENT_OUTPUT)
+      val jsonObject = mapper.readValue(stringJson, classOf[Object])
+      mapper.writeValueAsString(jsonObject)
 
-  def printInTextField: Unit =
-    console.setText("")
-    if r1.isSelected then
-      console.append(jsonPrettyFormatted(UIConstants.structure))
-    if r2.isSelected then
-      console.append(jsonPrettyFormatted(UIConstants.country))
-    if r3.isSelected then
-      console.append(jsonPrettyFormatted(UIConstants.unit_kinds))
-    if r4.isSelected then console.append(jsonPrettyFormatted(UIConstants.unit))
-    if r5.isSelected then
-      console.append(jsonPrettyFormatted(UIConstants.boundaries))
-    if r6.isSelected then
-      console.append(jsonPrettyFormatted(UIConstants.relations))
+    override def printInTextField: Unit =
+      console.setText("")
+      if rdbStructure.isSelected then
+        console.append(jsonPrettyFormatted(UIConstants.structure))
+      if rdbCountry.isSelected then
+        console.append(jsonPrettyFormatted(UIConstants.country))
+      if rdbUnitKind.isSelected then
+        console.append(jsonPrettyFormatted(UIConstants.unit_kinds))
+      if rdbUnits.isSelected then
+        console.append(jsonPrettyFormatted(UIConstants.unit))
+      if rdbBoundaries.isSelected then
+        console.append(jsonPrettyFormatted(UIConstants.boundaries))
+      if rdbRelations.isSelected then
+        console.append(jsonPrettyFormatted(UIConstants.relations))
 
-  def addComponentsToPanel(components: List[JComponent]): Unit =
-    for component <- components do this.add(component)
+    override def addComponentsToButtonGroup(
+      radioButtons: Seq[JRadioButton]
+    ): Unit =
+      for radioButton <- radioButtons do this.buttonGroup.add(radioButton)
+
+    override def addComponentsToPanel(components: Seq[JComponent]): Unit =
+      for component <- components do this.add(component)
