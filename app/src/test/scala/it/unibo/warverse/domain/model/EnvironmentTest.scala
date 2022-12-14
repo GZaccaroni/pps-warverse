ef353ffb3ec766c47d0cbde238737cdeb1358afa
alexbaiardi <alex.baiardi99@gmail.com>
2022-12-14 17:03:24 +0100

    test: updated EnvironmentTest

--- app/src/test/scala/it/unibo/warverse/domain/model/EnvironmentTest.scala
+++ app/src/test/scala/it/unibo/warverse/domain/model/EnvironmentTest.scala
@@ -1,33 +1,38 @@
 package it.unibo.warverse.domain.model
 
 import it.unibo.warverse.domain.model.common.DomainExample
+import it.unibo.warverse.domain.model.common.DomainExample.countryA
+import it.unibo.warverse.domain.model.world.Relations.InterCountryRelations
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 
 class EnvironmentTest extends AnyFunSuite with Matchers:
-  val environment = DomainExample.environment
+  val environment: Environment = DomainExample.environment
 
   test("At the beginning the day must start at 0") {
     environment.day mustBe 0
   }
 
-  test("When a day passed the environment must be updated") {
+  test("Environment must be copied with new parameters correctly") {
     environment.copiedWith(day = 1).day mustBe 1
+    environment.copiedWith(day = 1).countries mustBe environment.countries
+    environment
+      .copiedWith(interCountryRelations = InterCountryRelations(Set.empty))
+      .interCountryRelations mustBe InterCountryRelations(Set.empty)
+    environment
+      .copiedWith(
+        Seq.empty,
+        InterCountryRelations(Set.empty)
+      )
+      .countries mustBe empty
   }
 
-  test("Army size must increase after adding new units") {
-    val country = environment.countries.find(_.id == "ID_A").get
-    val countryArmy = country.armyUnits
-    val newEnv = environment.replacingCountry(
-      country.addingArmyUnits(countryArmy)
-    )
-    newEnv.countries
-      .find(_.id == "ID_A")
-      .get
-      .armyUnits
-      .size mustBe (countryArmy.size * 2)
-  }
-
-  test("Size of the initial states must be 3") {
-    environment.countries.size mustBe (3)
+  test("A state should be replaced by the given one") {
+    val updatedCountry = countryA.addingCitizens(100)
+    environment
+      .replacingCountry(updatedCountry)
+      .countries must not contain countryA
+    environment
+      .replacingCountry(updatedCountry)
+      .countries must contain(updatedCountry)
   }
