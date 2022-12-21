0f69d775b93bbac217beb96d5dd6cb31e9986015
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-12-21 13:09:57 +0100

    test: Improved encoded samples test

---
+++ app/src/test/scala/it/unibo/warverse/data/samples/EncodedSamplesTest.scala
@@ -0,0 +1,29 @@
+package it.unibo.warverse.data.samples
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+
+class EncodedSamplesTest() extends AnyFunSuite with Matchers:
+  private val encodedSamples = EncodedSamples()
+
+  test("complete simulation config sample generation must succeed") {
+    checkResult(() => encodedSamples.complete)
+  }
+  test("country sample generation must succeed") {
+    checkResult(() => encodedSamples.country)
+  }
+  test("country relations sample generation must succeed") {
+    checkResult(() => encodedSamples.countryRelations)
+  }
+  test("army unit sample generation must succeed") {
+    checkResult(() => encodedSamples.armyUnit)
+  }
+  test("army unit kind sample generation must succeed") {
+    checkResult(() => encodedSamples.armyUnitKind)
+  }
+  test("country boundaries sample generation must succeed") {
+    checkResult(() => encodedSamples.countryBoundaries)
+  }
+  private def checkResult(fun: () => String): Unit =
+    noException mustBe thrownBy(fun)
+    fun() mustNot be(null)
